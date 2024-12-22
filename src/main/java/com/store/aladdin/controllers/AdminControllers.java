f64f2f661dfa987e7c65b3ceb60b894ecd30b370
kanhaiyaPandey <kanhaiyaPandey2232@gmail.com>
2024-12-22 15:42:36 +0530

    new commit

--- src/main/java/com/store/aladdin/controllers/AdminControllers.java
+++ src/main/java/com/store/aladdin/controllers/AdminControllers.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +27,7 @@ import com.store.aladdin.services.ProductService;
 import com.store.aladdin.services.UserService;
 import com.store.aladdin.utils.ResponseUtil;
 
+
 @RestController
 @RequestMapping("/api/admin")
 public class AdminControllers {
@@ -42,12 +45,14 @@ public class AdminControllers {
 
 
     
-    @PostMapping("/create-product")
+    @PostMapping(value = "/create-product", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createProduct(
-            @RequestPart("product") Product product,
-            @RequestPart("images") List<MultipartFile> images) {
+        @RequestPart("product") @Validated Product product,
+        @RequestPart(value = "images", required = false) List<MultipartFile> images) {
         try {
             // Upload images to Cloudinary
+            System.out.println("Received product: " + product);
+            System.out.println("Received images: " + (images != null ? images.size() : "No images"));
             List<String> imageUrls = new ArrayList<>();
             for (MultipartFile image : images) {
                 String imageUrl = imageUploadService.uploadImage(image);
@@ -80,7 +85,7 @@ public class AdminControllers {
         productService.deleteProduct(new ObjectId(productId)); 
         return ResponseUtil.buildResponse("Product deleted successfully", HttpStatus.OK);
     }
-}
+
 
 
         // Get all users
@@ -89,3 +94,4 @@ public class AdminControllers {
             List<User> users = userService.getAllUsers();
             return ResponseEntity.ok(users);
         }
+    }
