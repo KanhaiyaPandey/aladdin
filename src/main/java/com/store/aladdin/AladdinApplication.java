3fd37e5c51787bf6b77d0f28910acca3313d1eff
Kanhaiya Pandey <kanhaiyapandey2232@gmail.com>
2024-12-27 10:14:51 +0530

    adding devtools in the project

--- src/main/java/com/store/aladdin/AladdinApplication.java
+++ src/main/java/com/store/aladdin/AladdinApplication.java
@@ -1,15 +1,35 @@
 package com.store.aladdin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.store.aladdin.repository")
 public class AladdinApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(AladdinApplication.class);
+
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(AladdinApplication.class, args);
+		logger.info("Server is running. No compilation errors!");
+	} catch (Exception e) {
+		logger.error("Error during startup: ", e);
+	  }
 	}
+
+	    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            logger.info("Server has started successfully!");
+        };
+    }
 
 }
