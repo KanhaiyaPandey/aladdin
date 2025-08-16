01a067f0d6804f75a55521c35c0fbb62305ad4cf
Kanhaiya Pandey <kanhaiyapandey2232@gmail.com>
2025-08-16 11:17:33 +0530

    test cases

--- src/test/java/com/store/aladdin/AladdinApplicationTests.java
+++ src/test/java/com/store/aladdin/AladdinApplicationTests.java
@@ -1,12 +1,13 @@
 package com.store.aladdin;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AladdinApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
