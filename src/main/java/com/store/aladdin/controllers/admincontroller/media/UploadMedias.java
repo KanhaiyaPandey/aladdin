4a266106f9fd7aa6503ac19dd2ca2146f78fed08
Kanhaiya Pandey <kanhaiyapandey2232@gmail.com>
2025-12-14 15:19:16 +0530

    media upload fix

--- src/main/java/com/store/aladdin/controllers/admincontroller/media/UploadMedias.java
+++ src/main/java/com/store/aladdin/controllers/admincontroller/media/UploadMedias.java
@@ -11,10 +11,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.store.aladdin.models.Medias;
@@ -34,8 +31,7 @@ public class UploadMedias {
     private final MongoTemplate mongoTemplate;
 
     @PostMapping(value = MediaRoutes.UPLOAD_MEDIA, consumes = "multipart/form-data")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Map<String, Object>> uploadMultipleMedia(@RequestParam("media") MultipartFile[] files) {
+    public ResponseEntity<Map<String, Object>> uploadMultipleMedia(@RequestPart("media") MultipartFile[] files) {
         try {
             List<CompletableFuture<Medias>> futures = new ArrayList<>();
 
